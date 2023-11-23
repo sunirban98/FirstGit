4d7a014fd0d9d6f388a28fea9b1f250ffbb097c3
sunirban98 <sahasunirban@gmail.com>
2023-11-23 21:48:36 +0530

    adding 5th file

--- demo.js/dom.js
+++ demo.js/dom.js
@@ -43,11 +43,50 @@ var li=document.getElementsByTagName('li');
 //var secondItems=document.querySelector('.list-group-item:nth-child(2)');
 //secondItems.style.backgroundColor='green';
 //QUERYSELECTORALL//
-var odd=document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<odd.length;i++)
+//var odd=document.querySelectorAll('li:nth-child(odd)');
+//for(var i=0;i<odd.length;i++)
 {
-    odd[i].style.backgroundColor='green';
+    //odd[i].style.backgroundColor='green';
 }
+//TRAVERSE THE DOM//
+var itemList=document.querySelector('#items');
+//parentNode//
+//itemList.parentNode.style.backgroundColor='#f4f4f4';
+//parentElement//
+//itemList.parentElement.style.backgroundColor='#f4f4f4';
+//childNodes//
+//children//
+//itemList.children[1].style.backgroundColor='yellow';
+//firstChild//
+//firstElementChild//
+itemList.firstElementChild.textContent='Hello';
+//lastElementChild//
+//itemList.lastElementChild.textContent='Hello';
+//nextSibling//
+//nextElementSibling//
+//itemList.nextElementChild.textContent='Hello';
+//previousSibling/
+//previousElementSibling//
+///temList.previousElementSibling.textContent='Hello';
+//createElement//
+//Add a div
+var newDiv=document.createElement('div');
+//Create a class//
+newDiv.className='hello';
+//Add id
+newDiv.id='hello1';
+//Add attribute
+newDiv.setAttribute('title','Hello Div');
+//Create text node//
+var newDiv=document.createTextNode('Hello World');
+//Add text to div
+newDiv.appendChild(newDivText);
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+console.log(newDiv);
+
+container.insertBefore(newDiv,h1);
+
 
 
 
