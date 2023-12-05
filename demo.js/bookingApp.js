38a2ac65811734089e7a6f864795f4330b79dd0b
sunirban98 <sahasunirban@gmail.com>
2023-12-05 09:38:35 +0530

    Adding 9th File

--- demo.js/bookingApp.js
+++ demo.js/bookingApp.js
@@ -4,9 +4,21 @@ function saveToLocalStorage(event)
     const name=document.getElementById('name').value;
     const mail=document.getElementById('mail').value;
     const number=document.getElementById('number').value;
-    localStorage.setItem('Name',name);
-    localStorage.setItem('Email ID',mail);
-    localStorage.setItem('Phone Number',number);
-
+    //localStorage.setItem('Name',name);
+    //localStorage.setItem('Email ID',mail);
+    //localStorage.setItem('Phone Number',number);
+    const obj={
+        name,
+        mail,
+        number
     }
+    localStorage.setItem(obj.mail,JSON.stringify(obj));
+    showUserOnScreen(obj)
+}
+function showUserOnScreen(obj){
+    const parentElement=document.getElementById('listOfitems');
+    const childElement=document.createElement('li');
+    childElement.textContent=obj.name+'-'+obj.mail+'-'+obj.number;
+    parentElement.appendChild(childElement);
+}
     
